5ef154d6ecc985e99d6fe47e4b1561447c2f2c3a
Ayush Rana <ayushrana2482@gmail.com>
2026-03-28 10:41:59 +0530

    Add files via upload

--- CH3/task3.js
+++ CH3/task3.js
@@ -1,33 +1,40 @@
-// Write node.js script to print “Welcome to Home Page” with two links containing two pages named as “About Us” and “Contact Us” on home page of server. If user request for About Us page it should display “Welcome to LJ University” in bold font-style with blue color and if user request for Contact Us page it should display “Email:abc@ljinstitutes.edu.in” in italic font-style with red color if any other request is requested it shows “Page not found” message in plaintext.
+// Write node js script to fetch values from url given below and display output as asked.
+// "https://www.google.com/exam.txt?c1=Hello&c2=FSD2 T1 Test&c3=Welcome to LJU#AllTheBest"
+// 1) Data must be written as below in file named “exam.txt”. File name must be fetched from the url given above.
+// Output:
+// Hello!
+// Welcome to LJU FSD2 T1 Test
+// #AllTheBest
+// 2) Read content from file “exam.txt” and send response to server and display data in “/” page in same format as above but in H1 tag and in red color.
+// 3) If any other page is requested it shows “Page not found” message in plain text.
 
-var http=require("http")
 var fs=require("fs")
 var url=require("url")
+var http=require("http")
+var add="https://www.google.com/exam.txt?c1=Hello&c2=FSD2 T1 Test&c3=Welcome to LJU#AllTheBest"
+var p=url.parse(add,true)
+var filename=p.pathname
+var content=`${p.query.c1}! \n${p.query.c3} ${p.query.c2} \n${p.hash}`
+fs.writeFileSync("."+filename,content)
 http.createServer((req,res)=>{
-    if(req.url==='/'){
-        res.writeHead(200,{
-            "content-type":"text/html"
-        })
-        res.end(`<h1>Welcome to Home Page</h1> <br>
-                <a href="/AboutUs">About</a>
-                <a href="/ContactUs">Contact</a>`)
+    if(req.url==="/home"){
+        var r=fs.readFileSync("."+filename,"utf-8")
+        res.writeHead(200,{"content-type":"text/html"})
+        res.end(`<h1 style="color:red"><pre>${r}</pre></h1>`)
     }
-    else if(req.url==="/AboutUs"){
-        res.writeHead(200,{
-            "content-type":"text/html"
-        })
-        res.write(`<h1 style="color: blue;font-weight: bold">Welcome to LJ University</h1>`)
+    else if(req.url==="/about"){
+        var a=fs.readFileSync("image.jpg")
+        res.writeHead(200,{"content-type":"image/jpg"})
+        res.end(a)
     }
-    else if(req.url==="/ContactUs"){
-        res.writeHead(200,{
-            "content-type":"text/html"
-        })
-        res.write(`<h1 style="color: red;font-style: italic">Email:abc@ljinstitutes.edu.in</h1>`)
+    else if(req.url==="/api"){
+        var d={"name":"rahul","course":"NodeJS","Enrolled":"yes"}
+         res.writeHead(200,{"content-type":"application/json"})
+         res.end(JSON.stringify(d))
     }
     else{
-           res.writeHead(404,{
-            "content-type":"text/plain"
-        })
-        res.end("Page not Found")
+        res.writeHead(404,{"content-type":"text/plain"})
+        res.end("Page Not Found")
     }
-}).listen(8007)
+}).listen(7005)
+process.noDeprecation=true
