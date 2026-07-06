aee77eb1f71d14a5dea6873bf75c3c0c85654477
Ayush Rana <ayushrana2482@gmail.com>
2026-07-06 10:39:33 +0530

    Add files via upload

---
+++ CH10/mg2.js
@@ -0,0 +1,29 @@
+const mg=require("mongoose")
+mg.connect("mongodb://127.0.0.1:27017/mdb2").then(()=>{console.log("Success")}).catch((e)=>{console.log(e)})
+const myschema=new mg.Schema(
+    {
+        username:String,
+        age:Number,
+        active:Boolean,
+        doj:{
+            type:Date,
+            default:new Date().toLocaleDateString()
+        }
+    },{strict:false,versionKey:false}
+)
+const mymodel=new mg.model("person",myschema)
+const newfun= async()=>{
+    try{
+        const data=[{
+            username:"ABC",age:28,active:false},
+            {username:"PQR",age:29,active:true},
+            {username:'XYZ',age:30,active:false}
+        ]
+        const result=await mymodel.insertMany(data)
+        console.log(result)
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+newfun()
---
+++ CH10/mg3.js
@@ -0,0 +1,52 @@
+const mg=require("mongoose")
+const validator=require("validator")
+mg.connect("mongodb://127.0.0.1:27017/mdb2")
+const myschema=({
+    name:{type:String,maxlength:[8,"Maximum length should be 8"],
+        minlength:4,required:true,
+        uppercase:true,trim:true,
+        match:[/^[A-Z a-z]+[0-9]+$/,"must start with letter and end with digit"]
+    },
+    email:{
+        required:true,unique:true,
+        type:String,
+        match:/\S+@\S+\.\S+/
+    },
+    alternateemail:{
+        type:String,
+        validate:[validator.isEmail,"Not Valid"]
+    },
+    age:{
+        type:Number,
+        max:48,
+        min:18
+    },
+    role:{
+        type:String,
+        enum:["buyer","seller"],
+        default:"buyer"
+    },
+    product:{
+        type:String,
+        validate:[validator.isAlphanumeric,"Not valid alphanumeric code!"]
+    }
+})
+const mymodel=new mg.model("products",myschema)
+const validfun=async()=>{
+    try{
+        const data=new mymodel({
+            name:"XYZ123",
+            email:"xyz2@gmail.com",
+            alternateemail:"x@gmail.com",
+            age:25,
+            role:"seller",
+            product:"myproduct1"
+    })
+    const result=await data.save()
+    console.log(result)
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+validfun()
